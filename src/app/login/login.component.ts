07dd42e08ac2ebe144528563b3e6448147d2a1af
Gabriel Caraballo Moya <backend1peruapps@gmail.com>
2019-06-27 17:39:00 -0500

    Mejoras en menú lateral. Diseño de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { UsuarioService } from '../servicios/usuarios/usuario.service';
 import { NgForm } from '@angular/forms';
 import { Usuario } from '../modelos/usuario.model';
 import { Title } from '@angular/platform-browser';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -17,11 +18,13 @@ export class LoginComponent implements OnInit {
   constructor(
     public router: Router,
     public _usuarioService: UsuarioService,
-    public title: Title
+    public title: Title,
+    @Inject(DOCUMENT) private document: Document
   ) { }
 
   ngOnInit() {
     this.title.setTitle( this.tituloOriginal );
+    this.document.body.classList.add('bodywall');
   }
 
   ingresar( forma: NgForm ) {
--- src/app/paginas/paginas.component.ts
+++ src/app/paginas/paginas.component.ts
@@ -1,18 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 declare function init_plugins();
 
 @Component({
   selector: 'app-paginas',
   templateUrl: './paginas.component.html',
-  styleUrls: ['./paginas.component.css']
+  styleUrls: ['./paginas.component.css'],
+  
 })
 export class PaginasComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    @Inject(DOCUMENT) private document: Document
+  ) { }
 
   ngOnInit() {
     init_plugins();
+    this.document.body.classList.remove('bodywall');
   }
 
 }
