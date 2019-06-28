44ea81be950898d05bdb70c2a54d80f36ffbe981
Gabriel Caraballo Moya <backend1peruapps@gmail.com>
2019-06-28 17:16:54 -0500

    Instalación de SweetAlert 2. Actualización de marca y grupo de productos

--- src/app/paginas/productos/productos.component.ts
+++ src/app/paginas/productos/productos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductosService } from 'src/app/servicios/productos/productos.service';
 import { URL_SERVICIOS } from 'src/app/config/config';
+import swal from'sweetalert2';
 
 @Component({
   selector: 'app-productos',
@@ -13,6 +14,7 @@ export class ProductosComponent implements OnInit {
   paginaSiguiente: string;
   paginaAnterior: string;
   total: number;
+  tituloAlerta = 'Correcto';
 
   constructor(
     public _productosService: ProductosService,
@@ -25,6 +27,22 @@ export class ProductosComponent implements OnInit {
         this.paginaAnterior = resp.data.prev_page_url;
         this.paginaSiguiente = resp.data.next_page_url;
       });
+  }
+
+  actualizarProducto( producto: any, text = 'Dato'){
+    this._productosService.actualizarProducto( producto )
+    .subscribe( resp => {
+      const Toast = swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 2000
+      });      
+      Toast.fire({
+        type: 'success',
+        title: '<span style="text-align: left">Dato actualizado correctamente: <br> ' + text + ' de producto</div>'
+      });
+    });
   }
 
   ngOnInit() {
--- src/app/servicios/productos/productos.service.ts
+++ src/app/servicios/productos/productos.service.ts
@@ -3,6 +3,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { tokenName } from '@angular/compiler';
 import { UsuarioService } from '../usuarios/usuario.service';
+import { URL_SERVICIOS } from 'src/app/config/config';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,6 @@ export class ProductosService {
   token: String;
   httpOptions: any;
   
-
   constructor(
     public http: HttpClient,
     public _usuarioService: UsuarioService,
@@ -20,17 +20,26 @@ export class ProductosService {
     this.httpOptions = {
       headers: new HttpHeaders({
         'Content-Type':  'application/json',
-        'Authorization': 'Bearer ' + _usuarioService.token
+        'Authorization': 'Bearer ' + _usuarioService.token,
       })
     };
   }
 
   cargarProductos(url: string): any {
-    console.log(this.token);
+    console.log(this.httpOptions);
     return this.http.get( url, this.httpOptions )
     .pipe(
       map( (resp: any) => {
-        console.log( resp.data.data );
+        return resp;
+      })
+    );
+  }
+
+  actualizarProducto(producto: any): any {
+    const url = URL_SERVICIOS + '/api/productos/' + producto.id;
+    return this.http.put( url, producto, this.httpOptions )
+    .pipe(
+      map( (resp: any) => {
         return resp;
       })
     );
